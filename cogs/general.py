1d0ac4d67b8ebcebfe9a4767b1ec493bc953abcd
W. Patrick Jones <wpmjones@gmail.com>
2019-11-06 08:02:14 -0500

    use coc.utils for clan tag correction

--- cogs/general.py
+++ cogs/general.py
@@ -1,3 +1,5 @@
+import coc
+
 from discord.ext import commands
 
 
@@ -8,16 +10,19 @@ class General(commands.Cog):
 
     @commands.command(name="get_clan", aliases=["getclan", "clan"])
     async def get_clan(self, ctx, clan_tag):
-        if not clan_tag.startswith("#"):
-            clan_tag = "#" + clan_tag
+        # This line uses a utility in the coc.py library to correct clan tags (case, missing #, etc.)
+        clan_tag = coc.utils.correct_tag(clan_tag)
+
         clan = await self.bot.coc.get_clan(clan_tag)
         content = f"The clan name for {clan_tag} is {clan.name}.\n"
         content += f"{clan.name} currently has {clan.member_count} members.\n\n"
+
         war = await self.bot.coc.get_current_war(clan_tag)
         if war:
             content += f"Current war state is {war.state}\n"
             if war.state != "notInWar":
                 content += f"Opponent: {war.opponent}"
+
         await ctx.send(content)
 
 

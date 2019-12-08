dafeedcf6a1523998a59b5cff08bb113cba6f4fc
W. Patrick Jones <wpmjones@gmail.com>
2019-12-07 22:15:19 -0500

    added command to retrieve Discord avatar

--- cogs/special.py
+++ cogs/special.py
@@ -1,3 +1,5 @@
+import discord
+
 from discord.ext import commands
 
 
@@ -6,7 +8,23 @@ class Special(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    # TODO add a different command here
+    @commands.command(name="avatar", hidden=True)
+    async def avatar(self, ctx, user: discord.Member = None):
+        """Command to see a larger version of the given member's avatar
+        Examples:
+        ++avatar @mention
+        ++avatar 123456789
+        ++avatar member#1234
+        """
+        if not user:
+            user = ctx.author
+        embed = discord.Embed(color=discord.Color.blue())
+        embed.add_field(name=f"{user.name}#{user.discriminator}", value=user.display_name, inline=True)
+        embed.add_field(name="Avatar URL", value=user.avatar_url, inline=True)
+        embed.set_image(url=user.avatar_url_as(size=128))
+        embed.set_footer(text=f"Discord ID: {user.id}",
+                         icon_url="https://discordapp.com/assets/2c21aeda16de354ba5334551a883b481.png")
+        response = await ctx.send(embed=embed)
 
 
 def setup(bot):
